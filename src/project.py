df76e1fd5b1f48cdd8b1e4bb22cd70ee0dda465c
Leslie Padilla <lpp230000@utdallas.edu>
2025-12-01 13:18:53 -0600

    setting up base for a game

--- src/project.py
+++ src/project.py
@@ -6,3 +6,16 @@ def main():
     pygame.display.set_caption("Waterfall")
     resolution = (800,600)
     screen = pygame.display.set_mode(resolution)
+
+    running = True
+    while running in pygame.event.get():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                #game logic
+                screen.fill('Green')
+                pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
