bbd27ca39cc24a41c4d612f498cb3b0f79b1b142
Leslie Padilla <lpp230000@utdallas.edu>
2025-12-01 13:26:40 -0600

    adding custom background

--- src/project.py
+++ src/project.py
@@ -7,13 +7,17 @@ def main():
     resolution = (800,600)
     screen = pygame.display.set_mode(resolution)
 
+    #making screen custom background
+    background = pygame.image.load ("waterfall_bg.png")
+    background = pygame.transform.scale (background, resolution)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
          #game logic
-        screen.fill('Green')
+        screen.blit(background, (0,0))
         pygame.display.flip()
     pygame.quit()
 
