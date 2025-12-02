41c3466bedc0967447dc9dd61c116434064c6a5a
Leslie Padilla <lpp230000@utdallas.edu>
2025-12-02 14:17:37 -0600

    reorganizing the code to divide using class

--- src/project.py
+++ src/project.py
@@ -1,76 +1,103 @@
 import pygame
 import random
 
-def main():
-    pygame.init()
-    pygame.display.set_caption("Waterfall")
-    resolution = (1000,800)
-    screen = pygame.display.set_mode(resolution)
-
-    #making screen custom background
-    background = pygame.image.load ("waterfall_bg.png")
-
-    #drawn platform
-    platform_height = 30
-    platform = pygame.Rect (0,
-     resolution[1] - platform_height, 
-     resolution[0], platform_height
-     )
+class Platform():
+    def __init__(self, x, y, width, height, color=(255,255,255)):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
     
-    #obstacle platforms
-    platforms =[]
-    num_of_platforms = 10
-    for i in range(num_of_platforms):
-        width = random.randint(80, 200)
-        height = 20
-        x = random.randint(0, resolution[0] - width)
-        y = random.randint(100, 700)
-        new_platforms = pygame.Rect(x, y, width, height)
-        platforms.append(new_platforms)
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
 
-    #character image
-    player = pygame.Rect(500, 600, 150 ,100)
-    player_speed = 6
-    gravity = 0.5
-    y_velocity = 0
-    ground = False
+class Player():
+    def __init__(self, x, y):
+        #character image
+        self.rect = pygame.Rect(500, 600, 150 ,100)
+        self.speed = 6
+        self.gravity = 0.5
+        self.y_velocity = 0
+        self.ground = False
+        self.img = pygame.image.load("waterfall_ball.png").convert_alpha()
+        self.img = pygame.transform.scale(self.img, (150, 100))
 
-    player_img = pygame.image.load("waterfall_ball.png").convert_alpha()
-    player_img = pygame.transform.scale(player_img, (150, 100))
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        #setting keyboard controls
-        keys = pygame.key.get_pressed()
+    def update (self, keys, platforms):
+        
         #Left/Right movement
         if keys[pygame.K_LEFT]:
-            player.x -= player_speed
+            self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
-            player.x += player_speed
-        #Jumping movement
-        if keys[pygame.K_SPACE] and ground:
-            y_velocity = -12
-            ground = False
-        #stops charact from floating (gravity)
-        y_velocity += gravity
-        player.y += y_velocity
-        # Ground Collision
-        if player.colliderect(platform) and y_velocity >= 0:
-            player.y = platform.y - player.height
-            y_velocity = 0
-            ground = True
+            self.rectect.x += self.speed
 
-         #game logic
-        screen.blit(background, (0,0))
-        pygame.draw.rect(screen, (150,75,0), platform)
+        #Jumping movement
+        if keys[pygame.K_SPACE] and self.ground:
+            self.y_velocity = -12
+            self.ground = False
+        #stops charact from floating (gravity)
+        self.y_velocity += self.gravity
+        self.rect.y += self.y_velocity
+        # Ground Collision
         for p in platforms:
-            pygame.draw.rect(screen, (255,255,255), p)
-        screen.blit(player_img, (player.x, player.y))
+            if self.rect.colliderect(p.rect) and self.y_velocity >= 0:
+                self.rect.y = p.rect.y - self.rect.height
+                self.y_velocity = 0
+                self.ground = True
     
-        pygame.display.flip()
-    pygame.quit()
+    def draw (self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+def main():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Waterfall")
+
+        self.resolution = (1000,800)
+        self.screen = pygame.display.set_mode(self.resolution)
+
+        #making screen custom background
+        self.background = pygame.image.load ("waterfall_bg.png")
+
+        #Ground platform
+        platform_height = 30
+        ground = Platform(0,
+        self.resolution[1] - platform_height, 
+        self.resolution[0], platform_height,
+        (150, 75, 0)
+        )
+        
+        #obstacle platforms
+        self.platforms =[ground]
+        num_of_platforms = 10
+        for i in range(num_of_platforms):
+            width = random.randint(80, 200)
+            height = 20
+            x = random.randint(0, self.resolution[0] - width)
+            y = random.randint(100, 700)
+            self.platforms.append(Platform(x, y, width, height, (255, 255, 255)))
+
+            #Player
+            self.player = Player(500, 600)
+            self.running = True
+
+        def run(self):
+            while running:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+
+                #setting keyboard controls
+                keys = pygame.key.get_pressed()
+                self.player.update(keys, self.platforms)
+                #game logic
+                self.screen.blit(self.background, (0,0))
+
+                for p in self.platforms:
+                    p.draw(self.screen)
+                
+                self.player.draw(self.screen)
+
+                pygame.display.flip()
+                
+        pygame.quit()
 
 
 if __name__ == "__main__":
