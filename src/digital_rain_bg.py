4808d89ff7c0820beed5534b8d86245657efab67
Leslie Padilla <lpp230000@utdallas.edu>
2025-12-03 11:59:44 -0600

    setting up main function and classes

--- src/digital_rain_bg.py
+++ src/digital_rain_bg.py
@@ -2,10 +2,20 @@ import pygame
 import random
 
 class Particle():
+    def __init__(self):
+        pass
 
 class ParticleTrail():
+    def __init__(self):
+        pass
 
 class Rain():
+    def __init__(self):
+        pass
 
 def main():
-    
+    pygame.init()
+    pygame.display.set_caption("Digital Rain Waterfall")
+
+if __name__ =="__main__":
+    main()
